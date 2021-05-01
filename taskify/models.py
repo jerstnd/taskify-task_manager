5d37252f5ef13dd2e02f0605409e1bcabc239c0b
Jeremy Rustandi <jeremyandika21@gmail.com>
2021-05-01 21:03:00 +0700

    making note and todo list models

--- taskify/models.py
+++ taskify/models.py
@@ -6,3 +6,14 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    todos = db.relationship('ToDo')
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo_list = db.Column(db.String(150))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.String(5000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- taskify/views.py
+++ taskify/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -7,7 +7,10 @@ views = Blueprint('views', __name__)
 def landing_page():
     return render_template('landingpage.html', user=current_user)
 
-@views.route('/home')
+@views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template ('home.html', user=current_user)
+    if request.method == 'POST':
+        lists = request.form.get('todo')
+        notes = request.form.get('notes')
+    return render_template('home.html', user=current_user)
